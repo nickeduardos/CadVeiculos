ae6ee70451177a925f6112ede7b9e7532ccda9fc
nickeduardos <nickeduardogk@gmail.com>
2026-09-16 21:55:05 -0300

    Iniciado a classe veiculos

---
+++ Veiculo.java
@@ -0,0 +1,48 @@
+public class Veiculo {
+    
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String placa;
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    } 
+    public String setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
+
+
+
+
+
+
+
+    
+}
+
+
+// ======= Cadastro de Veículos OO =======
+// 1 - Cadastrar Veículo
+// 2 - Listar Veículos
+// 3 - Consultar Veículo
+// 0 - Sair
